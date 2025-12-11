4c759f8b3b0f941a22c41bac6372c5cbe8e13dd5
Kavia AI <production@kavia.ai>
2025-12-11 19:18:33 +0000

    Checkpoint 1 - cg7cd41e52

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/xbox-game-manager-50610-50619/xbox_game_launcher_frontend
+flutter analyze
+
---
+++ xbox_game_launcher_frontend/android/app/src/main/kotlin/com/example/xbox_game_launcher_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.xbox_game_launcher_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
